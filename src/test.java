e11482ea016673c3d712938a6157ddb09f43a3ff
sebastian9654 <rodriguezseba2001@gmail.com>
2023-02-22 20:33:57 -0500

    first commit

---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        char[][] positionalGameBoard = { 
+            {'1', '|', '2', '|', '3'},
+            {'_',  '+', '_', '+', '_'},
+            {'4', '|', '5', '|', '6'},
+            {'_',  '+', '_', '+', '_'},
+            {'7', '|', '8', '|', '9'}
+        };
+        System.out.println(positionalGameBoard[0][4]);
+      
+}
